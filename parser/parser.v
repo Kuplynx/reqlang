37b9e1f44bd42f4b28b4ae77249552d2d552e722
Kupylnx <isaacsjohnson22@gmail.com>
2022-05-23 21:25:07 -0700

    base while I wait for the template from otto

---
+++ parser/parser.v
@@ -0,0 +1,10 @@
+module parser
+
+import os
+import net.http
+/* Parses a req file into arguments for the client*/
+
+
+pub fn parse_req(file_path string) ?http.FetchConfig {
+	
+}
--- reqlang.v
+++ reqlang.v
@@ -1,5 +1,20 @@
 module main
 
+
+/* Using net.http for now, will write my own http client later */
+// import net.http
+import cli 
+import os
+
 fn main() {
-	println('Hello World!')
+	mut app := cli.Command{
+		name: "reqlang"
+		description: "The reqlang interpreter written in V"
+		execute: fn (cmd cli.Command) ? {
+			mut file := cmd.args[0]
+			println(os.read_file(file) ?)
+		}
+	}
+	app.setup()
+	app.parse(os.args)
 }
